28dd6590df6023c87b220d82a44d024415b64823
chandanp619 <chandanp619@gmail.com>
2019-09-04 23:08:45 +0530

    add files build

---
+++ build/precache-manifest.208c2e88a155f3da0cd582ee14325587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df838222a9b8766e76a3ebb34743a935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bece16e1026e2fbb449a",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "0038c99ebe089e0b323e",
+    "url": "/static/css/main.8262990b.chunk.css"
+  },
+  {
+    "revision": "bece16e1026e2fbb449a",
+    "url": "/static/js/2.f902f737.chunk.js"
+  },
+  {
+    "revision": "0038c99ebe089e0b323e",
+    "url": "/static/js/main.88938f87.chunk.js"
+  },
+  {
+    "revision": "de463be5d5aa0ad86ed5",
+    "url": "/static/js/runtime~main.d37a8383.js"
+  }
+]);
